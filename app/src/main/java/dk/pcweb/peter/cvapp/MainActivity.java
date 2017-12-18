f50334bb07d0080a7af9163b2d79d7d369e360a0
Peter sodborg christensen <Peter@Peters-MacBook-Pro.local>
2017-12-18 14:56:28 +0100

    first commit

---
+++ app/src/main/java/dk/pcweb/peter/cvapp/MainActivity.java
@@ -0,0 +1,13 @@
+package dk.pcweb.peter.cvapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
